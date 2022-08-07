aa3882112dbd73699911fa6616cbe814625026be
StereoFlo <stereoflo@stereoflo.ru>
2022-08-07 16:09:53 +0300

    refactor

--- http/login_handler.go
+++ http/login_handler.go
@@ -22,12 +22,12 @@ func NewLoginHandler(userApp application.UserApp, responder *infrastructure.Resp
 	}
 }
 
-func (authInterface *LoginHandler) Login(context *gin.Context) {
+func (handler *LoginHandler) Login(context *gin.Context) {
 	var user *entity.User
 	var token *entity.Token
 	err := context.ShouldBindJSON(&user)
 	if err != nil {
-		context.JSON(http.StatusUnprocessableEntity, authInterface.responder.Fail("Invalid json provided"))
+		context.JSON(http.StatusUnprocessableEntity, handler.responder.Fail("Invalid json provided"))
 		return
 	}
 	validateUser := user.Validate("login")
@@ -37,15 +37,15 @@ func (authInterface *LoginHandler) Login(context *gin.Context) {
 	}
 
 	passwordRaw := user.Password
-	user, err = authInterface.UserApp.GetUserByEmail(user.Email)
+	user, err = handler.UserApp.GetUserByEmail(user.Email)
 	if err != nil {
-		context.JSON(http.StatusNotFound, authInterface.responder.Fail("user not found"))
+		context.JSON(http.StatusNotFound, handler.responder.Fail("user not found"))
 		return
 	}
 
 	err = infrastructure.VerifyPassword(user.Password, passwordRaw)
 	if err != nil {
-		context.JSON(http.StatusNotFound, authInterface.responder.Fail("password is wrong"))
+		context.JSON(http.StatusNotFound, handler.responder.Fail("password is wrong"))
 		return
 	}
 	now := time.Now()
@@ -56,7 +56,7 @@ func (authInterface *LoginHandler) Login(context *gin.Context) {
 		RefreshTokenExpire: now.Add(time.Hour * 16),
 		UserId:             user.ID,
 	}
-	authInterface.UserApp.SaveToken(token)
-	user, err = authInterface.UserApp.GetUser(user.ID, 1)
-	context.JSON(http.StatusOK, authInterface.responder.Success(token))
+	handler.UserApp.SaveToken(token)
+	user, err = handler.UserApp.GetUser(user.ID, 1)
+	context.JSON(http.StatusOK, handler.responder.Success(token))
 }
--- http/user_handler.go
+++ http/user_handler.go
@@ -21,53 +21,53 @@ func NewUserHandler(userApp application.UserApp, responder *infrastructure.Respo
 	}
 }
 
-func (userStr *UserHandler) SaveUser(context *gin.Context) {
+func (handler *UserHandler) SaveUser(context *gin.Context) {
 	var user entity.User
 	if err := context.ShouldBindJSON(&user); err != nil {
-		context.JSON(http.StatusBadRequest, userStr.responder.Fail(gin.H{
+		context.JSON(http.StatusBadRequest, handler.responder.Fail(gin.H{
 			"invalid_json": "invalid json",
 		}))
 		return
 	}
-	newUser, err := userStr.App.SaveUser(&user)
+	newUser, err := handler.App.SaveUser(&user)
 	if err != nil {
-		context.JSON(http.StatusBadRequest, userStr.responder.Fail(err))
+		context.JSON(http.StatusBadRequest, handler.responder.Fail(err))
 		return
 	}
-	context.JSON(http.StatusCreated, userStr.responder.Success(newUser.GetUser()))
+	context.JSON(http.StatusCreated, handler.responder.Success(newUser.GetUser()))
 }
 
-func (userStr *UserHandler) GetList(context *gin.Context) {
+func (handler *UserHandler) GetList(context *gin.Context) {
 	users := entity.Users{}
 	limit, _ := strconv.Atoi(context.Query("limit"))
 	if limit == 0 {
 		limit = 10
 	}
 	offset, _ := strconv.Atoi(context.Query("offset"))
-	cnt, _ := userStr.App.GetUserCount()
+	cnt, _ := handler.App.GetUserCount()
 	if cnt == 0 {
-		context.JSON(http.StatusOK, userStr.responder.SuccessList(0, limit, offset, gin.H{}))
+		context.JSON(http.StatusOK, handler.responder.SuccessList(0, limit, offset, gin.H{}))
 		return
 	}
 	var err error
-	users, err = userStr.App.GetList(limit, offset)
+	users, err = handler.App.GetList(limit, offset)
 	if err != nil {
-		context.JSON(http.StatusInternalServerError, userStr.responder.Fail(err.Error()))
+		context.JSON(http.StatusInternalServerError, handler.responder.Fail(err.Error()))
 		return
 	}
-	context.JSON(http.StatusOK, userStr.responder.SuccessList(cnt, limit, offset, users))
+	context.JSON(http.StatusOK, handler.responder.SuccessList(cnt, limit, offset, users))
 }
 
-func (userStr *UserHandler) GetUser(context *gin.Context) {
+func (handler *UserHandler) GetUser(context *gin.Context) {
 	userId, err := strconv.Atoi(context.Param("user_id"))
 	if err != nil {
 		context.JSON(http.StatusBadRequest, err.Error())
 		return
 	}
-	user, err := userStr.App.GetUser(userId, 1)
+	user, err := handler.App.GetUser(userId, 1)
 	if err != nil {
-		context.JSON(http.StatusInternalServerError, userStr.responder.Fail(err.Error()))
+		context.JSON(http.StatusInternalServerError, handler.responder.Fail(err.Error()))
 		return
 	}
-	context.JSON(http.StatusOK, userStr.responder.Success(user.GetUser()))
+	context.JSON(http.StatusOK, handler.responder.Success(user.GetUser()))
 }
