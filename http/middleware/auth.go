d83fd1fe2dd2aac204defaa399029e71bc856bff
StereoFlo <stereoflo@stereoflo.ru>
2022-10-05 13:18:46 +0300

    fixes

--- http/middleware/auth.go
+++ http/middleware/auth.go
@@ -21,7 +21,7 @@ func NewAuth(userApp application.UserApp, responder *infrastructure.Responder) *
 func (userApp *Auth) Auth(c *gin.Context) {
 	token := c.Request.Header.Get("X-ACCOUNT-TOKEN")
 	if token == "" {
-		c.JSON(401, userApp.responder.Fail("unauthorized"))
+		c.JSON(401, userApp.responder.Fail("Unauthorized"))
 		c.Abort()
 		return
 	}
@@ -35,12 +35,12 @@ func (userApp *Auth) Auth(c *gin.Context) {
 	}
 	dbToken, err := userApp.userApp.GetTokenByUId(data.Data.TokenId)
 	if err != nil {
-		c.JSON(401, userApp.responder.Fail(err))
+		c.JSON(401, userApp.responder.Fail("Token was not found"))
 		c.Abort()
 		return
 	}
 	if dbToken.AccessTokenExpire.Unix() < time.Now().Unix() {
-		c.JSON(401, userApp.responder.Fail("jwt-token is expired"))
+		c.JSON(401, userApp.responder.Fail("Token is expired"))
 		c.Abort()
 		return
 	}
