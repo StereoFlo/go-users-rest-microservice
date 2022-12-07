3041b3dff35a891259288162bb3f1b4fb60013e5
StereoFlo <stereoflo@users.github.com>
2022-12-07 17:09:27 +0300

    responder refactoring

--- infrastructure/responder.go
+++ infrastructure/responder.go
@@ -2,38 +2,41 @@ package infrastructure
 
 import "github.com/gin-gonic/gin"
 
-type Responder struct{}
+type Responder struct {
+	Meta map[string]any `json:"meta"`
+	Data any            `json:"data"`
+}
 
 func NewResponder() *Responder {
 	return &Responder{}
 }
 
-func (s Responder) Success(data interface{}) gin.H {
-	return gin.H{
-		"meta": gin.H{
-			"success": true,
-		},
-		"data": data,
+func (s Responder) Success(data interface{}) Responder {
+	s.Meta = gin.H{
+		"success": true,
 	}
+	s.Data = data
+
+	return s
 }
 
-func (s Responder) SuccessList(total int, limit int, offset int, data any) gin.H {
-	return gin.H{
-		"meta": gin.H{
-			"success": true,
-			"total":   total,
-			"limit":   limit,
-			"offset":  offset,
-		},
-		"data": data,
+func (s Responder) SuccessList(total int, limit int, offset int, data any) Responder {
+	s.Meta = gin.H{
+		"success": true,
+		"total":   total,
+		"limit":   limit,
+		"offset":  offset,
 	}
+	s.Data = data
+
+	return s
 }
 
-func (s Responder) Fail(data any) gin.H {
-	return gin.H{
-		"meta": gin.H{
-			"success": false,
-		},
-		"data": data,
+func (s Responder) Fail(data any) Responder {
+	s.Meta = gin.H{
+		"success": false,
 	}
+	s.Data = data
+
+	return s
 }
